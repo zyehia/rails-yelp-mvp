9865ff9599ff2f1e71236624139f6c1286e8dce6
Ziyad Yehia <ziyadyehia93@gmail.com>
2020-11-11 14:28:36 +0000

    Seeded the database with 5 valid restaurants

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+5.times do 
+  restaurant = Restaurant.create(name: Faker::Restaurant.unique.name, 
+                        address: "#{Faker::Address.unique.street_address}, #{Faker::Address.city}, #{Faker::Address.postcode}",
+                        category: ["chinese", "italian", "japanese", "french", "belgian"].sample,
+                        phone_number: Faker::PhoneNumber.cell_phone)
+end
