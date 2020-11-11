af39d2616e9b5d45f3101fb733cfa8f6a961ec63
Ziyad Yehia <ziyadyehia93@gmail.com>
2020-11-11 16:24:59 +0000

    The app now works and we can add reviews and restaurants

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,17 @@
 class ReviewsController < ApplicationController
+  def new
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @review = Review.new(restaurant_id: @restaurant.id)
+  end
+
+  def create 
+    review = Review.new(strong_params)
+    review.restaurant_id = params[:restaurant_id]
+    review.save
+    redirect_to restaurant_path(params[:restaurant_id])
+  end
+
+  def strong_params 
+    params.require(:review).permit(:rating, :content, :restaurant)
+  end
 end
