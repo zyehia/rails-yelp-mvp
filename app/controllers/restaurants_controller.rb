e0671fa52178c145d0533446c1d0b8bb7a83d59f
Ziyad Yehia <ziyadyehia93@gmail.com>
2020-11-11 16:07:33 +0000

    Can now add a new restaurant

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -7,4 +7,18 @@ class RestaurantsController < ApplicationController
     @restaurant = Restaurant.find(params[:id])
     @reviews = @restaurant.reviews
   end
+
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def create
+    restaurant = Restaurant.create(strong_params)
+    redirect_to restaurant_path(restaurant)
+  end
+
+  private
+  def strong_params
+    params.require(:restaurant).permit(:name, :address, :category)
+  end
 end
